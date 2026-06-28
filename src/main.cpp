06a5bd45e88dde049ee833cce76c0f699fff3435
Edd12321 <sanduedi309@gmail.com>
2026-06-28 16:54:24 +0300

    Add (?) $e, $f

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@
 #include <iterator>
 #include <memory>
 #include <stdexcept>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 #if __cplusplus < 201402L
@@ -118,6 +120,8 @@ void eval(preprocessor& pre) {
 	};
 	std::vector<block> stk(1);
 	std::unordered_set<std::string> consts;
+	std::unordered_map<std::string, std::string> var2float;
+	std::unordered_map<std::string, std::unordered_set<std::string>> type2vars;
 	std::string str;
 
 	while (pre >> str) {
@@ -147,7 +151,9 @@ void eval(preprocessor& pre) {
 				if (str == "$.")
 					break;
 				if (stk.size() != 1)
-					throw std::runtime_error("constant declared inside block\n");
+					throw std::runtime_error("constant " + str + " declared inside block\n");
+				if (consts.find(str) != consts.end())
+					throw std::runtime_error("constant " + str + " already exists\n");
 				consts.insert(std::move(str));
 			}
 	
@@ -159,14 +165,70 @@ void eval(preprocessor& pre) {
 					break;
 				arr.insert(std::move(str));
 			}
+			if (arr.size() <= 1)
+				throw std::runtime_error("need at least two vars for disjoint condition\n");
 			for (auto it = arr.begin(); it != arr.end(); ++it) {
 				for (auto jt = std::next(it); jt != arr.end(); ++jt) {
 					stk.back().disjs[*it].insert(*jt);
 					stk.back().disjs[*jt].insert(*it);
 				}
 			}
+		
+		} else {
+			// Floating hypothesis
+			std::string str2;
+			pre >> str2;
+			if (str2 == "$f") {
+				std::string typecode, var;
+				pre >> typecode >> var >> str2;
+				if (str2 != "$.")
+					throw std::runtime_error("unclosed floating hypothesis\n");
+				bool found = false;
+				for (auto const& it : stk) {
+					if (it.vars.find(var) != it.vars.end()) {
+						found = true;
+						break;
+					}
+				}
+				if (!found)
+					throw std::runtime_error("no such variable " + var + '\n');
+				if (consts.find(typecode) == consts.end())
+					throw std::runtime_error("no such constant " + typecode + '\n');
+				
+				if (var2float.find(var) != var2float.end())
+					throw std::runtime_error("typecode for " + var + " already exists\n");
+				var2float[var] = typecode;
+				
+				type2vars[typecode].insert(var);
+			
+
+			// Essential hypothesis
+			} else if (str2 == "$e") {
+				std::vector<std::string> seq;
+				std::string typecode;
+				pre >> typecode;
+				if (type2vars.find(typecode) == type2vars.end())
+					throw std::runtime_error("no such typecode " + typecode + '\n');
+				while (pre >> str) {
+					if (str == "$.")
+						break;
+					bool found = false;
+					for (auto const& it : stk) {
+						if (it.vars.find(str) != it.vars.end()) {
+							found = true;
+							break;
+						}
+					}
+					if (!found && consts.find(str) == consts.end())
+						throw std::runtime_error("no such symbol " + str + '\n');
+					seq.push_back(std::move(str));
+				}
+				// WIP
+			}
 		}
 	}
+	if (stk.size() != 1)
+		throw std::runtime_error("unclosed block\n");
 }
 
 int main(int argc, char **argv) {
