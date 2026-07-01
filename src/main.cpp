25835b9e940ddb17e315bba73c54a953494d5993
Edd12321 <sanduedi309@gmail.com>
2026-07-01 18:26:55 +0300

    Rudimentary `$a`

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cerrno>
+#include <cstddef>
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
@@ -124,11 +125,12 @@ void eval(preprocessor& pre) {
 		// Disjoint variable restrictions
 		std::unordered_map<std::string, std::unordered_set<std::string>> disjs;
 		// Floating hypotheses
-		std::unordered_map<std::string, std::string> var2float;
+		std::unordered_map<std::string, std::string> var2float, name2float;
 		std::unordered_map<std::string, std::unordered_set<std::string>> float2vars;
 		// Essential hypothesis
 		std::unordered_map<std::string, std::pair<std::string, std::vector<std::string>>> ess;
 	};
+	std::unordered_map<std::string, std::tuple<block, std::string, std::vector<std::string>>> ax, thm;
 	std::vector<block> stk(1);
 	auto find_varorconst = [&](std::string const& nam) {
 		for (auto it = stk.rbegin(); it != stk.rend(); ++it) {
@@ -140,7 +142,9 @@ void eval(preprocessor& pre) {
 	};
 	std::string str;
 	while (pre >> str) {
+		//
 		// Open/close a block
+		//
 		if (str == "${") {
 			stk.emplace_back();
 		} else if (str == "$}") {
@@ -148,7 +152,9 @@ void eval(preprocessor& pre) {
 				throw std::runtime_error("no block to close\n");
 			stk.pop_back();
 		
+		//
 		// Variable/constant
+		//
 		} else if (str == "$v" || str == "$c") {
 			char c = str[1];
 			while (pre >> str) {
@@ -159,12 +165,16 @@ void eval(preprocessor& pre) {
 				stk.back().varorconst.emplace(std::move(str), c);
 			}
 		
+		//
 		// Disjoint var condition
+		//
 		} else if (str == "$d") {
 			std::unordered_set<std::string> arr;
 			while (pre >> str) {
 				if (str == "$.")
 					break;
+				if (find_varorconst(str) != 'v')
+					throw std::runtime_error(str + " not a variable\n");
 				arr.insert(std::move(str));
 			}
 			if (arr.size() <= 1)
@@ -177,13 +187,22 @@ void eval(preprocessor& pre) {
 			}
 		
 		} else {
-			std::string name = std::move(str);
+			std::string typecode, name = std::move(str);
+			//
 			// Floating hypothesis
+			//
 			std::string str2;
-			pre >> str2;
+			pre >> str2 >> typecode;
+		
+			if (find_varorconst(typecode) != 'c')
+				throw std::runtime_error("no such typecode " + typecode + '\n');
+			
+			if (stk.back().name2float.find(name) != stk.back().name2float.end()
+			||  stk.back().ess.find(name) != stk.back().ess.end())
+				throw std::runtime_error("label " + name + " already exists in scope\n");
+
 			if (str2 == "$f") {
-				std::string typecode, var;
-				pre >> typecode;
+				std::string var;
 				while (pre >> var) {
 					if (var == "$.")
 						break;
@@ -191,31 +210,118 @@ void eval(preprocessor& pre) {
 						throw std::runtime_error("no such variable " + var + '\n');
 					if (stk.back().var2float.find(var) != stk.back().var2float.end())
 						throw std::runtime_error("floating hypothesis for " + var + " already exists\n");
+					stk.back().name2float[name] = typecode;
 					stk.back().var2float[var] = typecode;
 					stk.back().float2vars[typecode].insert(std::move(var));
 				}
 				
-
+			//
 			// Essential hypothesis
+			//
 			} else if (str2 == "$e") {
 				std::vector<std::string> seq;
-				std::string typecode, var;
-				pre >> typecode;
-				if (find_varorconst(typecode) != 'c')
-					throw std::runtime_error("no such typecode " + typecode + '\n');
-				while (pre >> var) {
-					if (var == "$.")
+				while (pre >> str) {
+					if (str == "$.")
 						break;
-					if (find_varorconst(var) == 'X')
-						throw std::runtime_error("no such symbol " + var + '\n');
-					seq.push_back(std::move(var));
+					if (find_varorconst(str) == 'X')
+						throw std::runtime_error("no such symbol " + str + '\n');
+					seq.push_back(std::move(str));
 				}
-				stk.back().ess[name] = std::move(std::make_pair(std::move(typecode), std::move(seq)));
-			
-			} else if (str2 == "$a") {
-				// WIP
-			} else if (str2 == "$p") {
-				// WIP
+				stk.back().ess[name] = std::move(std::make_pair(
+					std::move(typecode),
+					std::move(seq)));
+		
+			} else {
+				std::string stopstr;
+				if (str2 == "$a")
+					stopstr = "$.";
+				else if (str2 == "$p")
+					stopstr = "$=";
+				else throw std::runtime_error("bad token " + str2 + '\n');
+				
+				std::vector<std::string> seq;
+				std::unordered_set<std::string> strs;
+				std::unordered_map<std::string, std::ptrdiff_t> idx;
+				block b;
+				while (pre >> str) {
+					if (str == stopstr)
+						break;
+					seq.push_back(str);
+					strs.insert(std::move(str));
+				}
+				for (auto const& it : stk)
+					for (auto const& ess_it : it.ess)
+						for (auto const& str_it : ess_it.second.second)
+							strs.insert(str_it);
+				// we need:
+				// 1) b.ess
+				// 2) b.varorconst
+				// 3) b.disjs
+				// 4) b.var2float
+				// 5) b.float2vars
+				for (auto const& str : strs) {
+					for (std::ptrdiff_t i = stk.size() - 1; i >= 0; --i) {
+						auto fnd = stk[i].varorconst.find(str);
+						if (fnd != stk[i].varorconst.end()) {
+							// 2) b.varorconst
+							b.varorconst[str] = fnd->second;
+							idx[str] = i + 1; // not zero indexed
+							break;
+						}
+					}
+				}
+				for (std::ptrdiff_t i = stk.size() - 1; i >= 0; --i) {
+					// 1) b.ess
+					for (auto const& ess_it : stk[i].ess)
+						if (b.ess.find(ess_it.first) == b.ess.end())
+							b.ess[ess_it.first] = ess_it.second;
+					
+					// 3) b.disjs
+					for (auto const& disj_it : stk[i].disjs) {
+						auto const& var1 = disj_it.first;
+						auto const& var2s = disj_it.second;
+
+						auto fnd = idx.find(var1);
+						if (fnd != idx.end() && fnd->second <= i + 1) {
+							for (auto const& var2 : var2s) {
+								auto fnd = idx.find(var2);
+								if (fnd != idx.end() && fnd->second <= i + 1) {
+									b.disjs[var1].insert(var2);
+									b.disjs[var2].insert(var1);
+								}
+							}
+						}
+					}
+					
+					// 4) b.var2float && 5) b.float2vars
+					for (auto const& float_it : stk[i].var2float) {
+						auto const& var = float_it.first;
+						auto const& flt = float_it.second;
+
+						auto fnd_idx = idx.find(var);
+						if (fnd_idx != idx.end() && fnd_idx->second <= i + 1
+						&&  b.var2float.find(var) == b.var2float.end()) {
+								b.var2float.emplace(var, flt);
+								b.float2vars[flt].insert(var);
+						}
+					}
+				}
+
+				//
+				// Assertion
+				//
+				if (str2 == "$a") {
+					ax.emplace(name, std::move(std::make_tuple(
+						std::move(b),
+						std::move(typecode),
+						std::move(seq))));
+
+				//
+				// Proveable theorem
+				//
+				} else if (str2 == "$p") {
+					// WIP
+				}
 			}
 		}
 	}
